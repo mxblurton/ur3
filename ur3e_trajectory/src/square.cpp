7d334dbcbd9413eb134507978539ecd0a8d0fb61
mxblurton <mblurton@umd.edu>
2022-11-16 13:41:11 -0500

    aak

--- ur3e_trajectory/src/square.cpp
+++ ur3e_trajectory/src/square.cpp
@@ -30,12 +30,12 @@ int main(int argc, char **argv)
     moveit::planning_interface::MoveGroupInterface::Plan pose_plan;
     
     geometry_msgs::Pose pose_target;
-    pose_target.position.x = 0.4;
-    pose_target.position.y = 0.0;
-    pose_target.position.z = 1.1;
-    pose_target.orientation.x = -0.707;
-    pose_target.orientation.y = 0.0;
-    pose_target.orientation.z = 0.0;
+    pose_target.position.x = 0.54;
+    pose_target.position.y = 0.12;
+    pose_target.position.z = 1.04;
+    pose_target.orientation.x = 0;
+    pose_target.orientation.y = 0;
+    pose_target.orientation.z = 0.707;
     pose_target.orientation.w = 0.707;
 
     bool pose_plan_success;
@@ -47,6 +47,8 @@ int main(int argc, char **argv)
         arm_move_group.execute(pose_plan);
     }
 
+    ROS_INFO("Just moved to start location");
+
     // Create instance of cartesian plan
     moveit::planning_interface::MoveGroupInterface::Plan cartesian_plan;
 
@@ -54,9 +56,13 @@ int main(int argc, char **argv)
     geometry_msgs::Pose start_pose = arm_move_group.getCurrentPose().pose;
 
     geometry_msgs::Pose corner1_pose = start_pose;
-    corner1_pose.position.x += 0.2;
-    corner1_pose.position.y -= 0.2;
+    corner1_pose.position.x = -0.12;
+    corner1_pose.position.y = 0.54;
     //corner1_pose.position.z -= 0.2;
+    corner1_pose.orientation.x = 0;
+    corner1_pose.orientation.y = 0;
+    corner1_pose.orientation.z = 1.0;
+    corner1_pose.orientation.w = 0;
 
     // Define waypoints for the cartesian path
     std::vector<geometry_msgs::Pose> waypoints;
@@ -69,13 +75,19 @@ int main(int argc, char **argv)
     arm_move_group.execute(trajectory);
     n.setParam("/record_pose", false);
 
+    ROS_INFO("Just moved to second location");
+
     // Get the start Pose
     start_pose = arm_move_group.getCurrentPose().pose;
 
     geometry_msgs::Pose corner2_pose = start_pose;
-    corner2_pose.position.x -= 0.4;
-    corner2_pose.position.y += 0.4;
+    corner2_pose.position.x = -0.54;
+    corner2_pose.position.y = -0.12;
     //corner2_pose.position.z -= 0.2;
+    corner2_pose.orientation.x = 0;
+    corner2_pose.orientation.y = 0;
+    corner2_pose.orientation.z = -0.707;
+    corner2_pose.orientation.w = 0.707;
 
     // Define waypoints for the cartesian path
     std::vector<geometry_msgs::Pose> waypoints2;
@@ -86,5 +98,55 @@ int main(int argc, char **argv)
     n.setParam("/record_pose", true);
     arm_move_group.execute(trajectory);
     n.setParam("/record_pose", false);
+
+    ROS_INFO("Just moved to third location");
+
+    // Get the start Pose
+    start_pose = arm_move_group.getCurrentPose().pose;
+
+    geometry_msgs::Pose corner3_pose = start_pose;
+    corner3_pose.position.x = 0.12;
+    corner3_pose.position.y = -0.54;
+    //corner2_pose.position.z -= 0.2;
+    corner3_pose.orientation.x = 0;
+    corner3_pose.orientation.y = 0;
+    corner3_pose.orientation.z = 0;
+    corner3_pose.orientation.w = 1.0;
+
+    // Define waypoints for the cartesian path
+    std::vector<geometry_msgs::Pose> waypoints3;
+    waypoints3.push_back(corner3_pose);
+
+    trajectory = ArmController::planCartesianPath(start_pose, waypoints3, reference_frame, arm_move_group);
+
+    n.setParam("/record_pose", true);
+    arm_move_group.execute(trajectory);
+    n.setParam("/record_pose", false);
+
+    ROS_INFO("Just moved to fourth location");
+
+    // Get the start Pose
+    start_pose = arm_move_group.getCurrentPose().pose;
+
+    geometry_msgs::Pose corner4_pose = start_pose;
+    corner4_pose.position.x = 0.54;
+    corner4_pose.position.y = 0.12;
+    //corner2_pose.position.z -= 0.2;
+    corner4_pose.orientation.x = 0;
+    corner4_pose.orientation.y = 0;
+    corner4_pose.orientation.z = 0.707;
+    corner4_pose.orientation.w = 0.707;
+
+    // Define waypoints for the cartesian path
+    std::vector<geometry_msgs::Pose> waypoints4;
+    waypoints2.push_back(corner4_pose);
+
+    trajectory = ArmController::planCartesianPath(start_pose, waypoints4, reference_frame, arm_move_group);
+
+    n.setParam("/record_pose", true);
+    arm_move_group.execute(trajectory);
+    n.setParam("/record_pose", false);
+
+    ROS_INFO("Just moved to final location");
 
 }
